ad492dfd21b6bfa077f6c8d3ade9cd4924fb34f4
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-04 12:30:32 +0100

    Add signup sanitation

---
+++ controllers/signupController.js
@@ -0,0 +1,40 @@
+var User = require('../models/user');
+
+const { body, validationResult } = require('express-validator');
+
+exports.signup_get = function (req, res) {
+    res.render('signup_form', { title: 'Signup' });
+}
+
+exports.signup_post = [
+
+    //Validate and sanitize input
+    body('first_name', 'First name required').trim().isLength({ min: 1 }).escape(),
+    body('last_name', 'Last name required').trim().isLength({ min: 1 }).escape(),
+    body('email', 'Email address required').trim().isLength({ min: 1 }).escape(),
+    body('password', 'Password required').trim().isLength({ min: 1 }).escape(),
+
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('signup_form', { title: 'Signup', errors: errors.array() });
+            return;
+        }
+        
+        const user = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.email,
+        password: req.body.password,
+        member_status: 'basic_member'
+        
+    }).save(err => {
+        if (err) { 
+        return next(err);
+        }
+        res.redirect("/");
+    });
+}
+]
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var signup_controller = require('../controllers/signupController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/sign-up", signup_controller.signup_get);
+router.post("/sign-up", signup_controller.signup_post);
 
 module.exports = router;
