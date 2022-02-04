857c45454da879c6438cb8c8e28ddde5728741a8
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-04 10:51:20 +0100

    Add message model

---
+++ models/message.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var MessageSchema = new Schema({
+    title: { type: String, required: true, maxLength: 100 },
+    timestamp: { type: Date, default: Date.now},
+    text: { type: String, required: true, maxLength: 500 },
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+
+})
+
+
+// Virtual for message's URL
+UserSchema
+.virtual('url')
+.get(function () {
+  return '/message' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Message', MessageSchema);
