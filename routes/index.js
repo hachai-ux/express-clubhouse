f989bef9b3d5e5348749b83bd921f83ddec51c99
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-10 12:45:00 +0100

    Add REST for getting users and messages

--- routes/index.js
+++ routes/index.js
@@ -47,4 +47,63 @@ router.post("/new-message", message_controller.new_message_post);
 
 router.post("/delete-message", message_controller.delete_message_post);
 
+
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+
+
+
+router.get('/users', (req, res) => {
+  return res.send(Object.values(users));
+});
+
+router.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+router.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+router.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+
+router.post('/users', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+router.put('/users/:userid', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+router.delete('/users/:userid', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+
 module.exports = router;
